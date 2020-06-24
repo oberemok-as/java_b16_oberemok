b91cdda79103d27994cdf976a5338c3cc1c0e155
oberemok-as <8495@bk.ru>
2020-06-25 00:29:31 +0300

    добавление функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/mfp.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/mfp.java
@@ -1,9 +1,28 @@
 package ru.stqa.pft.sandbox;
 
+import java.util.Date;
+
 public class mfp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+  public static void main(String[] args) {
+    hello("World");
+    Date date = new Date();
+    System.out.println(date);
+    double len = 5;
+    System.out.println("Square = " + area(len));
+    double a=2;double b=3;
+    System.out.println("Square = " + area(a,b));
+  }
+
+  public static void hello(String user) {
+    System.out.println("Hello,  " + user + "!");
+  }
+
+  public static double area(double l){
+    return l*l;
+  }
+
+  public static double area(double a,double b){
+    return a*b;  }
 
 }
