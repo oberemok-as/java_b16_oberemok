35cd8b3cc11b1ba05e067539e285029e0fc6fb1a
oberemok-as <8495@bk.ru>
2020-06-24 22:33:03 +0300

    отвязка от Git

--- sandbox/src/main/java/ru/stqa/pft/sandbox/mfp.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/mfp.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class mfp {
 
-public static void main (String[] args){
-System.out.println("Hello World");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
 }
