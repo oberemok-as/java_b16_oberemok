c75bfbcc76e380236515bd370268907712500d31
oberemok-as <8495@bk.ru>
2020-07-08 01:35:27 +0300

    Задание 2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,20 +1,18 @@
 package ru.stqa.pft.sandbox;
 
 public class  Point {
-//объявление атрибутов
-  public double x;
-  public double y;
-//конструктор
+public double x;
+public double y;
+
   public Point(double x, double y) {
     this.x = x;
     this.y = y;
   }
-//функция
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow(p2.x-p1.x,2)+Math.pow(p2.y-p1.y,2));
+//! не просто x, а p2.x!
+  public double distance(Point p2) {
+    return Math.sqrt(Math.pow(p2.x - this.x, 2) + Math.pow(p2.y - this.y, 2));
   }
-
-  }
+}
 
 
 
