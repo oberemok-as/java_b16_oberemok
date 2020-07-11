a158dbdea86a21fb1e565dbb6db636825b3af3b9
oberemok-as <8495@bk.ru>
2020-07-12 01:26:26 +0300

    rename

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -3,10 +3,10 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
-public class ContactTestCase extends TestBase{
+public class ContactCreationTest extends TestBase{
 
   @Test
-  public void testContactTestCase() throws Exception {
+  public void testContactCreation() throws Exception {
     app.getNavigationHelper().link_add_new();
     app.getContactHelper().fill_form(new ContactData("Tester", "Testor", "Tester", "9269269269", "926@mail.ru", "100100 Sas str 18 18", "10", "May", "1989"));
     app.getContactHelper().submit_contact();
