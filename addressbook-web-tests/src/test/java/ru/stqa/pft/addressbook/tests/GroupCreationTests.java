8731e6dade0684e8133567c29622dc08e45b339c
oberemok-as <8495@bk.ru>
2020-07-21 23:20:42 +0300

    Лямбда-выражения (анонимные функции)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
+
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.HashSet;
@@ -13,21 +14,15 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("Admin", "GroupAdmin", "administration");
-       app.getGroupHelper().createGroup(group);
+    GroupData group = new GroupData("Admin1", "GroupAdmin", "administration");
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    Assert.assertEquals(after.size(),before.size()+1);
+    Assert.assertEquals(after.size(), before.size() + 1);
 
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
-int max=1;
-for (GroupData g : after){
-  if(g.getId()>max){
-    max=g.getId();
-  }
-}
-group.setId(max);
-before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<>(after));
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<>(after));
   }
 
 }
