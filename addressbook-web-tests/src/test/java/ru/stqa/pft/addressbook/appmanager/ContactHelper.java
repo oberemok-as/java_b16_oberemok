549249517412de6910311c77a00bec6dd60b4f07
oberemok-as <8495@bk.ru>
2020-07-25 22:49:18 +0300

    сравнение списков для creationContact

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -2,9 +2,13 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactHelper extends HelperBase {
 
@@ -72,5 +76,18 @@ return isElementPresent(By.name("selected[]"));
 
   public int getContactCount() {
     return wd.findElements(By.name("selected[]")).size();
+  }
+
+  public List<ContactData> getContactList() {
+     List<ContactData> contacts=new ArrayList<ContactData>();
+       List<WebElement> elements=wd.findElements(By.cssSelector("tr[name=\"entry\"]"));
+            for (WebElement element : elements){
+       List<WebElement> cells = element.findElements(By.tagName("td"));
+       String firstname = cells.get(2).getText();
+       String lastname = cells.get(1).getText();
+       ContactData contact = new ContactData(firstname,null,lastname,null,null,null,null,null,null,null,null);
+     contacts.add(contact);
+     }
+    return contacts;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -4,17 +4,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactCreationTest extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    int before = app.getContactHelper().getContactCount();
+    List<ContactData> before=app.getContactHelper().getContactList();
     app.getNavigationHelper().linkAddNew();
-    app.getContactHelper().createContact(new ContactData("Tester", "Testor", "Tester",
+    app.getContactHelper().createContact(new ContactData("Tester", "Testor", "Testerov",
             null, "9269269269", "926@mail.ru", "100100 Sas str 18 18",
             "10", "May", "1989", "Admin"), true);
-    int after = app.getContactHelper().getContactCount();
-    Assert.assertEquals(after, before + 1);
+    List<ContactData> after=app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() + 1);
   }
 
 
