2e0c5daef485402e8ec16c653692b2558bd0e998
oberemok-as <8495@bk.ru>
2020-09-05 14:40:44 +0300

    логирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -18,6 +20,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
 @DataProvider
 public Iterator<Object[]> validGroupsFromXML() throws IOException {
   try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))) {
@@ -51,7 +54,7 @@ public Iterator<Object[]> validGroupsFromXML() throws IOException {
 
   @Test(dataProvider = "validGroupsFromJSON")
   public void testGroupCreation(GroupData group) throws Exception {
-      app.goTo().groupPage();
+app.goTo().groupPage();
       Groups before = app.group().all();
       app.group().create(group);
       assertThat(app.group().count(), equalTo(before.size() + 1));
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.AplicationManager;
 
-public class TestBase {
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
+public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
   protected static final AplicationManager app
           = new AplicationManager(System.getProperty("browser",BrowserType.CHROME));
 
@@ -19,5 +26,15 @@ public class TestBase {
   public void tearDown() throws Exception {
     app.stop();
   }
+@BeforeMethod
+  public void logTestStart(Method m,Object[] p){
+  logger.info("Start test" + m.getName() + " with parameters " + Arrays.asList(p));
 
+}
+
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestEnd(Method m){
+    logger.info("End test" + m.getName());
+  }
 }
