953cdb92fa5eaade3cc2853fbbc7791134c424b8
oberemok-as <8495@bk.ru>
2020-09-22 22:33:21 +0300

    для отчета

---
+++ mantis/build/resources/test/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname				= 'localhost';
+$g_db_type				= 'mysqli';
+$g_db_username			= 'root';
+$g_db_username			= 'root';
+$g_db_password			= '';
+$g_signup_use_captcha	= OFF;
+$g_phpMailer_methodd    = PHPMAILER_METHOD_SMTP;
+$g_smtp_host            = 'localhost';
+$g_crypto_master_salt     = 'Xm8meEr+KsTmlhMYKfNQHRu2Z7Aqs8d8UGqjVKPPLUU=';
